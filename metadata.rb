8501084cb2cadc93a6b9c53af6ab86c3bb830a38
Jason Rohwedder <jro@risk.io>
2014-02-12 15:03:53 -0600

    Limit to OSes that use DEBs or RPMs

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,10 @@ description      'Installs/Configures nessus'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+%w{redhat centos scientific fedora debian ubuntu amazon}.each do |os|
+  supports os
+end
+
 attribute "installer_file",
   :display_name => "Installer File",
   :description => "Path to Nessus installer file"
